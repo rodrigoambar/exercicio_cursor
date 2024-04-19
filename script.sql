54a74e6d8899ca7e2f3ab0854f75a762d0390de4
Rodrigo Cordeiro Soares <digo.cordeiro04@gmail.com>
2024-04-19 17:42:27 -0300

    feat(ap16): entrega exercicio 1.2

--- script.sql
+++ script.sql
@@ -27,4 +27,27 @@ DO $$
 		
 	END;
 $$
+-- 1.2 Escreva um cursor que exibe todos os nomes dos youtubers em ordem reversa. Para tal
+-- - O SELECT deverá ordenar em ordem não reversa
+-- - O Cursor deverá ser movido para a última tupla
+-- - Os dados deverão ser exibidos de baixo para cima
+DO $$
+	DECLARE
+	cur_reverse REFCURSOR;
+	tupla RECORD;
+	BEGIN
+		OPEN cur_reverse SCROLL FOR
+			SELECT youtuber
+			FROM tb_youtubers
+			ORDER BY youtuber;
+		MOVE LAST FROM cur_reverse;
+	LOOP
+		FETCH BACKWARD FROM cur_reverse INTO tupla;
+		EXIT WHEN NOT FOUND;
+		RAISE NOTICE '%', tupla.youtuber;
+	END LOOP;
+	CLOSE cur_reverse;
+END;
+$$
+
 	
